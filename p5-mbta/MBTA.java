8714704749f671a45b856df420c66d21b99d31a6
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-11-29 19:26:56 -0500

    fixed build (?)

--- p5-mbta/MBTA.java
+++ p5-mbta/MBTA.java
@@ -1,8 +1,17 @@
 import java.util.*;
+import java.io.Reader;
+import java.io.FileReader;
+import java.lang.Exception;
+import java.lang.RuntimeException;
 
 public class MBTA {
-    private Map<String, Train>     lineTrains = new HashMap<String, Train>();
-    private Map<String, Passenger> passengers = new HashMap<String, Passenger>();
+    // Keeps track of each of the passengers
+    private Map<Train, List<Station>>      trainLines        = new HashMap<Train, List<Station>>();
+    private Map<Train, Integer>            trainLocations    = new HashMap<Train, Integer>();
+    private Map<Train, List<Passenger>>    onTrainPassengers = new HashMap<Train, List<Passenger>>();
+
+    private Map<Station, List<Passenger>>  stationPassengers = new HashMap<Station, List<Passenger>>();
+    private Map<Passenger, Queue<Station>> boardingPlans     = new HashMap<Passenger, Queue<Station>>();
     
     // Creates an initially empty simulation
     public MBTA() { }
@@ -10,15 +19,46 @@ public class MBTA {
     // Adds a new transit line with given name and stations
     public void addLine(String name, List<String> stations) {
         Train train = Train.make(name);
-        train.setStations(stations);
-        lineTrains.put(name, train);
+        
+        // Contains both the forwards and backwards part, 
+        // so only need to increment in one direction
+        // aka no need to keep track of the running direction
+        List<Station> stops = new LinkedList<Station>();
+        // Puts the "forwards" part of the path 
+        for (String station : stations) {
+            Station stop = Station.make(station);
+            stops.add(stop);
+            if (!this.stationPassengers.containsKey(stop))
+                this.stationPassengers.put(stop, new LinkedList<Passenger>());
+        }
+            
+        
+        // Puts the "backwards" part of the path
+        for (int i = stations.size() - 2; i > 0; i--)
+            stops.add(Station.make(station));
+        
+        this.trainLines.put(train, stops);
+        this.trainLocations.put(train, 0);
+        this.onTrainPassengers.put(train, new LinkedList<Passenger>());
     }
 
     // Adds a new planned journey to the simulation
     public void addJourney(String name, List<String> stations) {
         Passenger passenger = Passenger.make(name);
-        passenger.setJourney(stations);
-        passengers.put(name, passenger);
+
+        Queue<Station> stops = new LinkedList<Station>();
+        // Puts the "forwards" part of the path 
+        for (String station : stations) {
+            Station stop = Station.make(station);
+            stops.add(stop);
+            if (!this.stationPassengers.containsKey(stop))
+                this.stationPassengers.put(stop, new LinkedList<Passenger>());
+        }
+
+        Station start = stops.remove();
+
+        this.stationPassengers.get(start).add(passenger);
+        this.boardingPlans.put(passenger, stops);
     }
 
     // Return normally if initial simulation conditions are satisfied, otherwise
@@ -33,12 +73,35 @@ public class MBTA {
 
     // reset to an empty simulation
     public void reset() {
-        lineTrains.clear();
-        passengers.clear();
+        trainLines.clear();
+        trainLocations.clear();
+        stationPassengers.clear();
+        onTrainPassengers.clear();
+        boardingPlans.clear();
     }
 
     // adds simulation configuration from a file
     public void loadConfig(String filename) {
-        throw new UnsupportedOperationException();
+        Map<String, Map<String, List<String>>> jsonData = new HashMap<String, Map<String, String>>();
+        try {
+            Reader jsonFile   = new FileReader(filename);
+            Gson   jsonReader = new Gson();
+
+            TypeToken<Map<String, Map<String, List<String>>>> mapType = new TypeToken<Map<String, Map<String, List<String>>>>();
+            jsonData = jsonReader.fromJson(jsonFile, mapType);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getCause());
+        }
+
+        if (!jsonData.containsKey("lines") || !jsonData.containsKey("trips"))
+            throw new RuntimeException("The JSON is not in the right format!");
+        
+        Map<String, List<String>> lines = jsonData.get("lines");
+        for (String line : lines.keySet()) 
+            this.addLine(line, lines.get(line));
+
+        Map<String, List<String>> trips = jsonData.get("trips");
+        for (String passenger : trips.keySet()) 
+            this.addJourney(passenger, trips.get(passenger));
     }
 }
--- p5-mbta/Passenger.java
+++ p5-mbta/Passenger.java
@@ -1,13 +1,9 @@
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Passenger extends Entity {
     private static Map<String, Passenger> passengerPool = new HashMap<String, Passenger>();
 
-    // Keeps track of which stations to go to
-    private Queue<Station> path = new ConcurrentLinkedQueue<Station>(); 
     private Passenger(String name) { super(name); }
 
     public static Passenger make(String name) {
@@ -19,16 +15,4 @@ public class Passenger extends Entity {
         return passenger;
     }
 
-    public void setJourney(List<String> journey) {
-        for (String station : journey) 
-            path.add(Station.make(station));
-    }
-
-    public Station nextStop() {
-        return path.peek();
-    }
-
-    public boolean isFinished() {
-        return path.isEmpty();
-    }
 }
