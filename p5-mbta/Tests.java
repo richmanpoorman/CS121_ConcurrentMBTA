251fec5f3f214dee61002afe5b8de17c26336c26
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-12-03 13:07:22 -0500

    edited the test

--- p5-mbta/Tests.java
+++ p5-mbta/Tests.java
@@ -8,19 +8,85 @@ public class Tests {
     @Test public void testTransferingLines() {
         assertTrue("true should be true", true);
     }
+    @Test 
+    public void test1() {
+        MBTA mbta = new MBTA();
+        Train rLine = Train.make("red");
+        Station davis = Station.make("Davis");
+        Station harvard = Station.make("Harvard");
+        Station kendall = Station.make("Kendall");
+
+        Passenger bob = Passenger.make("Bob");
+        
+        mbta.addLine("red", List.of("Davis", "Harvard", "Kendall"));
+        mbta.addJourney("Bob", List.of("Davis", "Kendall"));
+        
+        Log log = new Log();
+        
+        log.passenger_boards(bob, rLine, davis);
+        log.train_moves(rLine, davis, harvard);
+        
+        Verify.verify(mbta, log);
+    }
+
+
+    @Test 
+    public void testOrange() {
+        MBTA mbta = new MBTA();
+        Train orangeLine = Train.make("Orange");
+        Station forest_hills = Station.make("Forest Hills");
+        Station green_st = Station.make("Green Street");
+        Station stony_brook = Station.make("Stony Brook");
+        Station jackson_sq = Station.make("Jackson Square");
+        Station roxbury_xing = Station.make("Roxbury Crossing");
+
+        Passenger john_doe = Passenger.make("jd1");
+        
+        mbta.addLine("Orange", 
+            List.of("Forest Hills", "Green Street", "Stony Brook", 
+            "Jackson Square", "Roxbury Crossing"));
+        mbta.addJourney("jd1", List.of("Forest Hills", "Jackson Square"));
+        
+        Log log = new Log();
+        
+        log.passenger_boards(john_doe, orangeLine, forest_hills);
+        log.train_moves(orangeLine, forest_hills, green_st);
+        
+        Verify.verify(mbta, log);
+    }
+
+    @Test 
+    public void test2() {
+        MBTA mbta = new MBTA();
+        Train gLine = Train.make("green");
+        Station s1 = Station.make("s1");
+        Station s2 = Station.make("s2");
+        Station s3 = Station.make("s3");
+        Station s4 = Station.make("s4");
+        Passenger p1 = Passenger.make("p1");
+        mbta.addLine("green", List.of("s1", "s2", "s3", "s4"));
+        mbta.addJourney("p1", List.of("s1", "s3"));
+        Log log = new Log(List.of(new BoardEvent(p1, gLine, s1), 
+                                new MoveEvent(gLine, s1, s2),
+                                new MoveEvent(gLine, s2, s3),
+                                new DeboardEvent(p1, gLine, s3),
+                                new MoveEvent(gLine, s3, s4)));
+        Verify.verify(mbta, log);
+    }
 
     @Test public void verifyTransfer() {
         // Set up //
         MBTA mbta = new MBTA();
         
         List<String> lineA = Arrays.asList("1", "2", "3");
-        List<String> lineB = Arrays.asList("1", "3", "5");
+        List<String> lineB = Arrays.asList("0", "1", "3", "5");
         List<String> lineC = Arrays.asList("5", "4");
 
         List<String> riderA = Arrays.asList("1", "5", "4");
         List<String> riderB = Arrays.asList("2", "1", "5", "4");
         List<String> riderC = Arrays.asList("5", "3", "1", "2");
 
+        Station s0 = Station.make("0");
         Station s1 = Station.make("1");
         Station s2 = Station.make("2");
         Station s3 = Station.make("3");
@@ -45,29 +111,40 @@ public class Tests {
 
         // Journey //
         Log logger = new Log();
+        logger.train_moves(trainA, s1, s2);       // The trainA goes from "1" to "2"
+        logger.train_moves(trainB, s0, s1);       // The trainB goes from "0" to "1"
         logger.passenger_boards(a, trainB, s1);   // "a" gets on the trainB at "1"
         logger.train_moves(trainB, s1, s3);       // The trainB goes from "1" to "3"
-        logger.train_moves(trainA, s1, s2);       // The trainA goes from "1" to "2"
+        logger.train_moves(trainC, s5, s4);       // The trainC goes from "5" to "4"
+        logger.train_moves(trainB, s3, s5);       // The trainB goes from "3" to "5"
         logger.train_moves(trainA, s2, s3);       // The trainA goes from "2" to "3"
         logger.train_moves(trainA, s3, s2);       // The trainA goes from "3" to "2"
-        logger.train_moves(trainB, s3, s5);       // The trainB goes from "3" to "5"
         logger.passenger_deboards(a, trainB, s5); // "a" gets off of the trainB at "5"
         logger.passenger_boards(c, trainB, s5);   // "c" gets on the trainB at "5"
         logger.passenger_boards(b, trainA, s2);   // "b" gets on the trainA at "2"
         logger.train_moves(trainA, s2, s1);       // The trainA goes from "2" to "1"
-        logger.passenger_boards(a, trainC, s5);   // "a" getss on the trainC at "5"
+        logger.passenger_deboards(b, trainA, s1); // "b" gets oof of the trainA at "1"
         logger.train_moves(trainB, s5, s3);       // The trainB goes from "5" to "3"
+        logger.train_moves(trainC, s4, s5);       // The trainC goes from "4" to "5"
+        logger.passenger_boards(a, trainC, s5);   // "a" gets on the trainC at "5"
         logger.passenger_deboards(c, trainB, s3); // "c" gets off of the trainB at "3"
         logger.passenger_boards(c, trainB, s3);   // "c" gets on the trainB at "3"
+        logger.train_moves(trainA, s1, s2);       // The trainA goes from "1" to "2"
         logger.train_moves(trainB, s3, s1);       // The trainB goes from "3" to "1"
+        logger.train_moves(trainA, s2, s3);       // The trainA goes from "2" to "3"
+        logger.train_moves(trainA, s3, s2);       // The trainA goes from "3" to "2"
         logger.passenger_deboards(c, trainB, s1); // "c" gets off of the trainB at "1"
         logger.passenger_boards(b, trainB, s1);   // "b" gets on the the trainB at "1"
-        logger.passenger_boards(c, trainA, s1);   // "c" gets on the trainA at "1"
         logger.train_moves(trainB, s1, s3);       // The trainB goes from "1" to "3"
+        logger.train_moves(trainA, s2, s1);       // The trainA goes from "2" to "1"
+        logger.passenger_boards(c, trainA, s1);   // "c" gets on the trainA at "1"
         logger.train_moves(trainA, s1, s2);       // The trainA goes from "1" to "2"
+        logger.train_moves(trainC, s5, s4);       // The trainC goes from "5" to "4"
         logger.train_moves(trainB, s3, s5);       // The trainB goes from "3" to "5"
         logger.passenger_deboards(b, trainB, s5); // "b" gets off of the trainB at "5"
+        logger.train_moves(trainB, s5, s3);       // The trainB goes from "5" to "3"
         logger.passenger_deboards(c, trainA, s2); // "c" gets off of the trainA at "2" (DONE)
+        logger.train_moves(trainC, s4, s5);       // The trainC goes from "4" to "5"
         logger.passenger_boards(b, trainC, s5);   // "b" gets on the trainC at "5"
         logger.train_moves(trainC, s5, s4);       // The trainC goes from "5" to "4"
         logger.passenger_deboards(b, trainC, s4); // "b" gets off of the trainC at "4" (DONE)
